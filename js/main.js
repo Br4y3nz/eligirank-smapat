d0d11792f297dccdd7ecbb290582614ad635106c
Br4y3nz <Syahrir508254@gmail.com>
2025-04-22 20:45:40 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/dist/supabase.min.js';
-
-  // Initialize Supabase client
+ // Initialize Supabase client
   const supabaseUrl = `https://yauwsxvgjmmyleheclpi.supabase.co`; // Replace with your Supabase project URL
   const supabaseKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhdXdzeHZnam1teWxlaGVjbHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDY3NjUsImV4cCI6MjA2MDQ4Mjc2NX0.sIXEAS4gW2WLB7vk_359Jp3QB6R9NT3Qv9gGdE9u2JY`;
   const supabase = createClient(supabaseUrl, supabaseKey);
--- js/server.js
+++ js/server.js
@@ -120,7 +120,6 @@ app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = 'https://yauwsxvgjmmyleheclpi.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhdXdzeHZnam1teWxlaGVjbHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDY3NjUsImV4cCI6MjA2MDQ4Mjc2NX0.sIXEAS4gW2WLB7vk_359Jp3QB6R9NT3Qv9gGdE9u2JY';
