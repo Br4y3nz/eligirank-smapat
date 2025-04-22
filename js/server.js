abc7a72fabc9969c9185d2dacfd1aea2cb1b0371
Br4y3nz <Syahrir508254@gmail.com>
2025-04-22 20:46:57 +0800

    update

--- js/server.js
+++ js/server.js
@@ -121,10 +121,6 @@ app.listen(PORT, () => {
 });
 
 
-const supabaseUrl = 'https://yauwsxvgjmmyleheclpi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhdXdzeHZnam1teWxlaGVjbHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDY3NjUsImV4cCI6MjA2MDQ4Mjc2NX0.sIXEAS4gW2WLB7vk_359Jp3QB6R9NT3Qv9gGdE9u2JY';
-const supabase = createClient(supabaseUrl, supabaseKey);
-
 // User Registration Endpoint
 app.post('/api/register', async (req, res) => {
     const { email, password } = req.body;
