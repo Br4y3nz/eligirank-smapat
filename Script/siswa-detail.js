428a590d1c0238ce24ded8c166cb98e168ddde74
Br4y3nz <syahrir508254@gmail.com>
2025-07-26 19:04:46 +0800

    .

--- Script/siswa-detail.js
+++ Script/siswa-detail.js
@@ -44,8 +44,12 @@ function tampilkanRapor(data) {
       <td>${item.nilai}</td>
       <td>${konversiGrade(item.nilai)}</td>
       <td>
-        <button class="btn-edit-mapel" data-id="${item.id}" data-mapel-id="${item.mapel_id}" data-semester="${item.semester}">Edit</button>
-        <button class="btn-delete-mapel" data-id="${item.id}">Hapus</button>
+        <button class="btn-edit-mapel" data-id="${item.id}" data-mapel-id="${item.mapel_id}" data-semester="${item.semester}" aria-label="Edit Mapel">
+          &#9998;
+        </button>
+        <button class="btn-delete-mapel" data-id="${item.id}" aria-label="Hapus Mapel">
+          &#128465;
+        </button>
       </td>
     `;
     total += item.nilai;
