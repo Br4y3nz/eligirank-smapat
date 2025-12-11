beb9fc1ba5c266cc6d1ce1773d0c137c4ce79259
Br4y3nz <syahrir508254@gmail.com>
2025-12-11 15:55:17 +0800

    Auth

--- Script/register.js
+++ Script/register.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.0.0/+esm';
 
 const supabaseUrl = 'https://yauwsxvgjmmyleheclpi.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlhdXdzeHZnam1teWxlaGVjbHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDY3NjUsImV4cCI6MjA2MDQ4Mjc2NX0.sIXEAS4gW2WLB7vk_359Jp3QB6R9NT3Qv9gGdE9u2JY';
+const supabaseKey = 'sb_publishable_vaduWdniIYxzUJMnyxBjGg_Wkrsn_OP';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function signUp() {
